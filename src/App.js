7a707f9c599b1699f07cea3df05ace637dbd93e8
NicolasRiosPulido <45575584+NicolasRiosPulido@users.noreply.github.com>
2021-04-21 11:55:37 -0500

    estilos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 function App() {
-  return <div>
-    <h1>test webhook</h1>
-  </div>;
+  const handleClick = () => {
+    console.log('click')
+  }
+  return (
+    <div className='contenedor'>
+      <h1>test webhook</h1>
+      <button onClick={handleClick}>Comprar</button>
+    </div>
+  );
 }
 
 export default App;

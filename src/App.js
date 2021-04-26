aae796c8ffcd72bbdf9c54e544b23451c74415f7
NicolasRiosPulido <45575584+NicolasRiosPulido@users.noreply.github.com>
2021-04-26 11:26:39 -0500

    integracion . backend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const handleClick = () => {
-    console.log('click')
+  const handleClick = async () => {
+    const result = await axios.get('https://wcbkctlgn3.execute-api.us-east-2.amazonaws.com/dev/new?id=test4&tipo=Compra')
+    const { data: { data: { tipo } } } = result
+    alert('El evento es: ' + JSON.stringify(tipo))
   }
+  const handleSubmit = async () => {
+    const result = await axios.get('https://wcbkctlgn3.execute-api.us-east-2.amazonaws.com/dev/new?id=test4&tipo=Landing')
+    const { data: { data: { tipo } } } = result
+    alert('El evento es: ' + JSON.stringify(tipo))
+  }
+  useEffect(() => {
+    handleSubmit()
+  }, [])
+
   return (
     <div className='contenedor'>
       <h1>test webhook</h1>
